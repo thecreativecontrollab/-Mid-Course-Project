84774851bdaa1427eb355e0874740c0024dd3223
The Creative Control Lab <43919740+thecreativecontrollab@users.noreply.github.com>
2019-05-28 15:08:17 -0500

    Add files via upload

--- src/PairOfDiceTest.java
+++ src/PairOfDiceTest.java
@@ -1,18 +1,69 @@
 import	 static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
 
 class PairOfDiceTest {
 
-	@Test
-	public void test() {
-		int chance = PairOfDice.rollDice();
-		System.out.println(chance);
-		assertTrue(chance >= 1 && chance <= 6 );
+	PairOfDice die1 = new PairOfDice();
+
+
+//
+//	@Test
+//	public void test() {
+//		int chance = PairOfDice.rollDice();
+//		System.out.println(chance);
+//		assertTrue(chance >= 1 && chance <= 6 );
+//
+//	}
+//
+//	@Test 
+//	public void testDieRoll() {
+//		int dieRoll = PairOfDice.roll();
+//		System.out.println(dieRoll);
+//		equals(dieRoll(0));
+//
+////	}
+//
+//	@Test
+//	public void rollTotalWin() {
+//		assertEquals(10, rollTotalLose(5 + 5));
+//		assertEquals(7, rollTotalLose(2 + 5));
+//	}
+//
+//	private Integer rollTotalWin(int i) {
+//		// TODO Auto-generated method stub
+//		return i ;
+//	}
+//
+//
+//	@Test
+//	public void rollTotalLose() {
+//		assertEquals(2, rollTotalLose(1 + 1));
+//		assertEquals(3, rollTotalLose(2 + 1));
+//		assertEquals(12, rollTotalLose(6 + 6));
+//		
+//	}
+//
+//	private Integer rollTotalLose(int i) {
+//		// TODO Auto-generated method stub
+//		return i ;
+//	}
+	
+	
+	@Test 
+	public void pointTotal() {
+		assertEquals(6, pointTotal(6 == 6)); 
 		
 	}
 
 
-
+private Integer pointTotal(boolean b) {
+	Integer pointtotal = null;
+	// TODO Auto-generated method stub
+	return pointtotal;
 }
+}
+
 
